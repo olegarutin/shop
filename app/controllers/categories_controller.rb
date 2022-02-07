cb6d513ea62d075f5fc4704f918cd19c4ba29af7
oleg <o1eg.34ac@gmail.com>
2022-02-07 23:29:14 +0200

    Show products by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+  before_action :set_category
+
+  def show
+    @pagy, @products = pagy(Product.where(category_id: @category), items: 8)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
