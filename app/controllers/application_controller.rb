0b35d65523ea243af53f58dc439ad6abe5efd2fe
oleg <o1eg.34ac@gmail.com>
2022-02-08 17:16:58 +0200

    Write constant SORTING_TYPE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  SORTING_TYPE = { 'Low-price': Product.order(price: :asc),
+                   'High-price': Product.order(price: :desc),
+                   'From A to Z': Product.order(title: :asc),
+                   'From Z to A': Product.order(title: :desc) }.freeze
 end
