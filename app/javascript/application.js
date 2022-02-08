4d11574ae0b052e7373aa3c05dce00ac120513e2
oleg <o1eg.34ac@gmail.com>
2022-02-08 17:13:44 +0200

    Connect rails ujs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
+import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+Rails.start()
